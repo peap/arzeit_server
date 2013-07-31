69eb2913bc4f33a3ce84c7926e387da2f65d03b3
Eric Petersen <eric@huskers.unl.edu>
2013-07-30 23:11:35 -0500

    adding category models

--- categories/models.py
+++ categories/models.py
@@ -1,3 +1,49 @@
-from django.db import models
+from datetime import timedelta
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Category(models.Model):
+
+    user = models.ForeignKey(User)
+
+    parent = models.ForeignKey('self',
+        blank=True,
+        null=True,
+        db_index=True,
+    )
+
+    name = models.CharField(
+        max_length=100,
+    )
+
+    description = models.CharField(
+        max_length=1000,
+        blank=True,
+        null=True,
+    )
+
+    @property
+    def today(self):
+        """
+        Total time in this category today.
+        """
+        total = timedelta(0)
+        for category in self.category_set.all():
+            total += category.today
+        for timer in self.timer_set.all():
+            total += timer.today
+        return total
+
+    @property
+    def last_week(self):
+        """
+        Total time in this category last week (a week is Monday to Sunday).
+        """
+        total = timedelta(0)
+        for category in self.category_set.all():
+            total += category.last_week
+        for timer in self.timer_set.all():
+            total += timer.last_week
+        return total
