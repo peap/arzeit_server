20541d6276040afb351c9287e785cba93c3aa213
Eric Petersen <eric@huskers.unl.edu>
2013-07-30 23:30:40 -0500

    adding rest_framework urls

--- arzeit_server/urls.py
+++ arzeit_server/urls.py
@@ -1,11 +1,41 @@
 from django.conf.urls import patterns, include, url
+from django.contrib.auth.models import User, Group
+from rest_framework import viewsets, routers
+
+from categories.models import Category
+from tags.models import Tag
+from timers.models import Timer, Interval
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
 
+
+class UserViewSet(viewsets.ModelViewSet):
+    model = User
+class GroupViewSet(viewsets.ModelViewSet):
+    model = Group
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    model = Category
+class TagViewSet(viewsets.ModelViewSet):
+    model = Tag
+class TimerViewSet(viewsets.ModelViewSet):
+    model = Timer
+class IntervalViewSet(viewsets.ModelViewSet):
+    model = Interval
+
+router = routers.DefaultRouter()
+router.register(r'users', UserViewSet)
+router.register(r'groups', GroupViewSet)
+router.register(r'categories', CategoryViewSet)
+router.register(r'tags', TagViewSet)
+router.register(r'timers', TimerViewSet)
+router.register(r'intervals', IntervalViewSet)
+
 urlpatterns = patterns('',
 
+    url(r'^', include(router.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 
     # Examples:
