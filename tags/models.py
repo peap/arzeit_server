7574cea1d1cedca3c0da0024204060cf852fe378
Eric Petersen <eric@huskers.unl.edu>
2013-07-30 23:13:05 -0500

    adding tag models

--- tags/models.py
+++ tags/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Tag(models.Model):
+
+    user = models.ForeignKey(User)
+
+    name = models.CharField(
+        verbose_name='Name',
+        max_length=25,
+    )
+
+    description = models.CharField(
+        max_length=1000,
+        blank=True,
+        null=True,
+    )
